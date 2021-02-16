2cd8ccfc7a4185f40faa870c7bb52c6f4c72c148
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    sm8350-common: Migrate to Oplus power AIDL HAL
    Change-Id: I22dd0baaf1594db1650bccf37b4d167b50135174

--- common.mk
+++ common.mk
@@ -317,9 +317,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power-service.oplus-libperfmgr
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -368,7 +366,9 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/oplus
+    hardware/oplus \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
