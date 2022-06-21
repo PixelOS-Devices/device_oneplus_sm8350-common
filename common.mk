925fe1e95e5dbcedac6f066360947384954fcdf0
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-06-21 19:58:50 -0400

    sm8350-common: Declare support for blur
    The Adreno 660 should be more than capable of handling this even though
    we know that there is a slight performance hit. However, this shouldn't
    matter all that much as blur isn't really used in a lot of places.
    
    This also enables a runtime toggle in Developer Options that disables it
    in case someone doesn't want it.
    
    Change-Id: I20d9599bd76d11ceb685875268a44b6363576666
    Reviewed-on: https://review.statixos.com/c/android_device_asus_sake/+/7999
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- common.mk
+++ common.mk
@@ -160,6 +160,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.supports_background_blur=1
+
 # Doze
 PRODUCT_PACKAGES += \
     OplusDoze
