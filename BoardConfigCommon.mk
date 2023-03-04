9a7157a645d24673e38413309d52a2b4a5a52ead
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-04 23:47:50 +0600

    sm8350-common: introduce a stub lahaina-post-boot script
    This will be used to tune some parameters in the device. Only run
    this script after proprietary kernel post-boot has run, so that
    these changes are not overriden by the same.
    
    Change-Id: Ibcef27a2813870c62d54c9d762eae9e03da677ad
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,6 +180,7 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 # SEPolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
