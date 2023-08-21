b6f045959d3de8c2b3d924cd8c11cbe05ed18435
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-21 13:21:18 +0530

    sm8350-common: Introduce DolbyManager
    - Converted Xiaomi dirac implementation based on observing
      Xiaomi sound effects app and daxservice.
    
    - Thanks to jhenrique09 for the hack from Xiaomi dirac parts
      "Pause/play music stream to get effects applied".
    
    Change-Id: I2de96ba526284dbbb7eb6c85d1963cf5937e8664
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common.mk
+++ common.mk
@@ -59,6 +59,9 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     sound_trigger.primary.lahaina:32
 
+PRODUCT_PACKAGES += \
+    DolbyManager
+
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8350/audio
 QCV_FAMILY_SKUS := lahaina yupik
 
