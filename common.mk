a8811975fd405b490a4bfa2a73cc5e85055f50cf
Sugakesshaa <sugakesshaa@pixelos.net>
2024-04-26 10:52:57 +0800

    sm8350-common: Inherit and build OplusDolby
    Change-Id: I4e4918b7da60f66ea669b457b3e837f3cb737bed

--- common.mk
+++ common.mk
@@ -125,6 +125,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+# Dolby
+$(call inherit-product, hardware/oplus/dolby/dolby.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
