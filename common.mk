520f6e29b9305fdc7d478cc78bfcde490e4f5283
LuK1337 <priv.luk@gmail.com>
2025-03-07 14:53:03 +0100

    Revert "sm8350-common: Remove audio_amplifier.lahaina from PRODUCT_PACKAGES"
    This reverts commit 66ab9ad7c82ddf4718ed9cab0981f48f95f33656.
    
    Change-Id: Ia16a4aa432c13fa2208a34688df93c62f2032bee

--- common.mk
+++ common.mk
@@ -41,6 +41,7 @@ PRODUCT_PACKAGES += \
     audio.primary.lahaina \
     audio.r_submix.default \
     audio.usb.default \
+    audio_amplifier.lahaina \
     audioadsprpcd \
     liba2dpoffload \
     libbatterylistener \
