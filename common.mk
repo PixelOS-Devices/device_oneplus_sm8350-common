951ee780c96d6c6d75cd217d4de5ae21d109f95b
Sugakesshaa <sugakesshaa@gmail.com>
2025-03-17 16:58:13 +0800

    sm8350-common: Set oplus camera packagename
    Change-Id: I880de52daa1c9a87bf09dfec12769b2994655183

--- common.mk
+++ common.mk
@@ -94,6 +94,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 $(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+TARGET_CAMERA_PACKAGE_NAME := com.oplus.packageName
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
